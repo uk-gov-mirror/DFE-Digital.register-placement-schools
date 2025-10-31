2c9329fbc692378b674c4558f63360bd799c85b3
defong <470137+defong@users.noreply.github.com>
2025-10-31 15:34:56 +0000

    [0257] Added service pattern

---
+++ app/lib/service_pattern.rb
@@ -0,0 +1,20 @@
+module ServicePattern
+  def self.included(base)
+    base.extend(ClassMethods)
+    base.class_eval do
+      private_class_method(:new)
+    end
+  end
+
+  def call
+    raise(NotImplementedError("#call must be implemented"))
+  end
+
+  module ClassMethods
+    def call(*args, **keyword_args, &block)
+      return new.call if args.empty? && keyword_args.empty? && block.nil?
+
+      new(*args, **keyword_args, &block).call
+    end
+  end
+end
